85a25ac1c7315882304c236023f0f22a6d67fa8d
Leancb <leancb@hotmail.com>
2025-08-13 03:12:41 -0300

    Correções

--- mobile-tests/wdio.conf.cjs
+++ mobile-tests/wdio.conf.cjs
@@ -5,21 +5,27 @@ exports.config = {
   runner: 'local',
   specs: ['./test/specs/**/*.js'],
   maxInstances: 1,
-  capabilities: [{
-    'appium:platformName': 'Fake',
-    'appium:automationName': 'Fake',
-    'appium:app': path.join(process.cwd(), 'fake-app.xml'), // << AQUI
-    'appium:deviceName': 'FakeDevice'
-  }],
   logLevel: 'info',
+  bail: 0,
   framework: 'mocha',
-  reporters: [
-    'spec',
-    ['junit', { outputDir: './reports', outputFileFormat: (o) => `results-${o.cid}.xml` }]
-  ],
-  mochaOpts: { ui: 'bdd', timeout: 60000 },
-  services: [],
+  mochaOpts: { timeout: 120000 },
+  connectionRetryTimeout: 120000,
+
+  // Conexão com o Appium (que o Actions vai subir)
   hostname: '127.0.0.1',
   port: 4723,
-  path: '/', // Appium 2
+  path: '/', // Appium 2 usa '/' por padrão
+
+  // NÃO use o service 'appium' aqui, pois o server subirá pelo workflow
+  // services: [],
+
+  capabilities: [{
+    'appium:platformName': 'Android',
+    'appium:automationName': 'UiAutomator2',
+    'appium:deviceName': 'Android Emulator',
+    'appium:platformVersion': '11', // Emulador API 30 (Android 11)
+    'appium:app': path.resolve(__dirname, 'app/app-debug.apk'), // <-- AJUSTE AQUI SE NECESSÁRIO
+    'appium:autoGrantPermissions': true,
+    'appium:newCommandTimeout': 120
+  }],
 };
